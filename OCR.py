4af4e16674682dce5ea4e732761ad6ad187165a3
Julian Kahnert <mail@juliankahnert.de>
2016-10-09 14:49:23 +0200

    initial create

---
+++ OCR.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import os
+
+base_path = os.path.dirname(os.path.realpath(__file__))
+
+src_path = base_path + '/in/'
+dst_path = base_path + '/out/'
---
+++ README.md
@@ -0,0 +1,1 @@
+ocrmypdf --jobs 2 --rotate-pages --deskew --clean --clean-final --force-ocr --output-type pdfa  --oversample 600 SOURCE DESTINATION
